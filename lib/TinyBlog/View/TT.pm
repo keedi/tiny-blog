41cd1030c345407c4beae9d290b30eec0a535d1d
keedi.k <keedi.k@cc9a5c7a-94df-11dd-9740-e5297458527a>
2008-10-08 16:58:31 +0000

    * View에서 사용할 기본 템플릿 파일 확장자 지정 * TT2용 markdown 필터 추가 * makrdown에서 제공하지 않는 시작 헤딩 수준 조정할 수 있게 함

--- lib/TinyBlog/View/TT.pm
+++ lib/TinyBlog/View/TT.pm
@@ -2,6 +2,23 @@ package TinyBlog::View::TT;
 
 use strict;
 use base 'Catalyst::View::TT';
+use Text::MultiMarkdown;
+
+{
+    no warnings qw/redefine/;
+    sub Text::MultiMarkdown::_GenerateHeader {
+        my ($self, $level, $id) = @_;
+
+        $level += ($self->{params}->{start_level} - 1);
+        return "<h$level>"  .  $self->_RunSpanGamut($id)  .  "</h$level>\n\n";
+    }
+}
+
+my $m = Text::MultiMarkdown->new(
+    tab_width     => 2,
+    use_wikilinks => 0,
+    start_level   => 3,
+);
 
 __PACKAGE__->config({
     INCLUDE_PATH => [
@@ -11,7 +28,15 @@ __PACKAGE__->config({
     PRE_PROCESS  => 'config/main',
     WRAPPER      => 'site/wrapper',
     ERROR        => 'error.tt2',
-    TIMER        => 0
+    TIMER        => 0,
+    TEMPLATE_EXTENSION => '.tt2',
+    FILTERS      => {
+        'markdown' => sub {
+            my $text = shift;
+            $text = $m->markdown($text);
+            return $text;
+        },
+    },
 });
 
 =head1 NAME
