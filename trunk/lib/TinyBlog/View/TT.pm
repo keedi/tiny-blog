d391a16841db25400a595498c8c88668bf135a4e
keedi.k <keedi.k@cc9a5c7a-94df-11dd-9740-e5297458527a>
2008-10-08 05:18:01 +0000

    * Add View with TTSite from Catalyst's helper script

---
+++ trunk/lib/TinyBlog/View/TT.pm
@@ -0,0 +1,41 @@
+package TinyBlog::View::TT;
+
+use strict;
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config({
+    INCLUDE_PATH => [
+        TinyBlog->path_to( 'root', 'src' ),
+        TinyBlog->path_to( 'root', 'lib' )
+    ],
+    PRE_PROCESS  => 'config/main',
+    WRAPPER      => 'site/wrapper',
+    ERROR        => 'error.tt2',
+    TIMER        => 0
+});
+
+=head1 NAME
+
+TinyBlog::View::TT - Catalyst TTSite View
+
+=head1 SYNOPSIS
+
+See L<TinyBlog>
+
+=head1 DESCRIPTION
+
+Catalyst TTSite View.
+
+=head1 AUTHOR
+
+Mooninchul,,,
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
+
