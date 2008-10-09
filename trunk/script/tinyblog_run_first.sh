f084f7eaf61abe2a4c51c2b286f506fc4195a8bc
keedi.k <keedi.k@cc9a5c7a-94df-11dd-9740-e5297458527a>
2008-10-09 01:49:08 +0000

    * 관리를 위해 sql 덤프하는 부분을 분리

--- trunk/script/tinyblog_run_first.sh
+++ trunk/script/tinyblog_run_first.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-sqlite3 tinyblog.db < tinyblog01.sql
-sqlite3 tinyblog.db < tinyblog02.sql
+script/tinyblog_dump_sql.sh
 
-script/tinyblog_create.pl model DB DBIC::Schema TinyBlog::Schema create=static dbi:SQLite:tinyblog.db
+#script/tinyblog_create.pl model DB DBIC::Schema TinyBlog::Schema create=static dbi:SQLite:tinyblog.db
