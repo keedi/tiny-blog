432d8569db6959cd4640b72a998bb077f5a17d3d
keedi.k <keedi.k@cc9a5c7a-94df-11dd-9740-e5297458527a>
2008-10-10 04:10:10 +0000

    * sql 파일 갱신

--- trunk/tinyblog01.sql
+++ trunk/tinyblog01.sql
@@ -1,23 +1,28 @@
 CREATE TABLE users (
-    id          INTEGER PRIMARY KEY,
+    id          INTEGER,
     username    TEXT,
     password    TEXT,
     nick        TEXT,
     email       TEXT,
-    active      INTEGER
+    active      INTEGER,
+
+    PRIMARY KEY ( id )
 );
 
 CREATE TABLE roles (
-    id          INTEGER PRIMARY KEY,
-    role        TEXT
+    id          INTEGER,
+    role        TEXT,
+
+    PRIMARY KEY ( id )
 );
 
 CREATE TABLE user_roles (
     user_id     INTEGER,
     role_id     INTEGER,
+
     PRIMARY KEY ( user_id, role_id )
 );
 
-INSERT INTO roles VALUES ( 1, 'reader' );
+INSERT INTO roles VALUES ( 1, 'admin' );
 INSERT INTO roles VALUES ( 2, 'writer' );
-INSERT INTO roles VALUES ( 3, 'admin' );
+INSERT INTO roles VALUES ( 3, 'reader' );
