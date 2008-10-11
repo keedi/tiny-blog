f46657d5159c65f0ad201e15e524c88285a10a95
keedi.k <keedi.k@cc9a5c7a-94df-11dd-9740-e5297458527a>
2008-10-11 03:09:21 +0000

    사용자 관리(조회, 등록, 제거) 스크립트 작성 완료

--- trunk/script/tinyblog_user.pl
+++ trunk/script/tinyblog_user.pl
@@ -8,12 +8,13 @@ use Path::Class;
 use lib dir($Bin, '..', 'lib')->stringify;
 
 use Config::General qw/ParseConfig/;
+use Digest::SHA;
 use TinyBlog::Schema;
 
 #
 # 명령행 인자 처리
 #
-die "Usage: $0 <action> <username> [<param1> <param2> ... ]"
+die usage()
     unless @ARGV >= 2;
 my ( $action, $username ) = @ARGV;
 
@@ -42,7 +43,7 @@ if ( $action eq 'view' ) {
         roles    => join(', ', map { $_->role } $user->roles),
     );
 
-    printf "%10s: %s\n", $_, $param_of{$_}
+    printf "%15s: %s\n", $_, $param_of{$_}
         for qw/ username nick email active roles /;
 }
 elsif ( $action eq 'add' ) {
@@ -51,23 +52,85 @@ elsif ( $action eq 'add' ) {
 
     my %param_of;
     $param_of{username} = $username;
-    for my $key ( qw/ nick email active roles / ) {
-        printf "%10s: ", $key;
+    for my $key ( qw/ password password_again nick email active roles / ) {
+        printf "%15s: ", $key;
         my $value = <STDIN>;
         chomp $value;
         $param_of{$key} = $value;
-    }
 
-    print "let's add... [TODO]\n";
+        die "Canceled [$action] process!\n"
+            if ($key eq 'password_again' && $param_of{password} ne $value);
+    }
+    $param_of{active}   = $param_of{active} == 1 ? 1 : 0;
+    $param_of{password} = Digest::SHA::sha1_hex($param_of{password});
+
+    print "Check the information...\n";
+    printf "%15s: %s\n", $_, $param_of{$_}
+        for qw/ username nick email active roles /;
+    print "Add this user? (yes/no): ";
+    my $confirm = <STDIN>;
+    chomp $confirm;
+
+    if ( $confirm =~ /^yes$/i ) {
+
+        my $user = $schema->resultset('Users')->create( {
+            username => $param_of{username},
+            password => $param_of{password},
+            nick     => $param_of{nick},
+            email    => $param_of{email},
+            active   => $param_of{active},
+        } );
+
+        my @role_names = map { s/^\s+//; s/\s+$//; $_ } split /,/, $param_of{roles};
+        for my $role_name ( @role_names ) {
+
+            my $role = $schema->resultset('Roles')->find({ role => $role_name });
+            next unless $role;
+
+            my $user_role = $schema->resultset('UserRoles')->create( {
+                user_id => $user->id,
+                role_id => $role->id,
+            } );
+        }
+    }
+    else {
+        die "Canceled [$action] process!\n";
+    }
 }
 elsif ( $action eq 'del' ) {
     my $user = $schema->resultset('Users')->find( { username => $username } )
         or die "Cannot find [$username]";
-    print "let's delete... [TODO]\n";
+
+    my %param_of = (
+        username => $user->username,
+        nick     => $user->nick,
+        email    => $user->email,
+        active   => $user->active,
+        roles    => join(', ', map { $_->role } $user->roles),
+    );
+
+    print "Check the information...\n";
+    printf "%15s: %s\n", $_, $param_of{$_}
+        for qw/ username nick email active roles /;
+    print "Delete this user? (yes/no): ";
+
+    my $confirm = <STDIN>;
+    chomp $confirm;
+    if ( $confirm =~ /^yes$/i ) {
+        $user->delete;
+    }
+    else {
+        die "Canceled [$action] process!\n";
+    }
 }
 else {
-    die
-        "Usage: $0 <action> <username>\n",
-        "    action: view, add, del\n";
+    die usage();
 }
 
+sub usage {
+    return <<"END_USAGE";
+Usage: $0 <action> <username>
+
+action: view, add, del
+END_USAGE
+}
