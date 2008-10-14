c1e50fca575b55f034ee06030e62f49fa0afedf0
keedi.k <keedi.k@cc9a5c7a-94df-11dd-9740-e5297458527a>
2008-10-14 01:48:35 +0000

    * 날짜별로 글을 보여줄 수 있는 저장소 기능 추가

--- trunk/lib/TinyBlog/Controller/Date.pm
+++ trunk/lib/TinyBlog/Controller/Date.pm
@@ -19,12 +19,37 @@ Catalyst Controller.
 
 =head2 index 
 
+날짜별 저장소 출력
+
 =cut
 
-sub index :Path :Args(0) {
-    my ( $self, $c ) = @_;
+sub index :Path :Args {
+    my ( $self, $c, $year, $month ) = @_;
 
-    #$c->response->body('Matched TinyBlog::Controller::Date in Date.');
+    if ( $year && !$month ) {
+        $c->stash->{date} = {
+            year  => $year,
+            str   => sprintf("%04d년", $year),
+        };
+        $c->stash->{posts}
+            = [ $c->model('DB')->get_posts_from_date( $year )  ];
+    }
+    elsif ( $year && 1 <= $month && $month <= 12 ) {
+        $c->stash->{date} = {
+            year  => $year,
+            month => $month,
+            str   => sprintf("%04d년 %02d월", $year, $month),
+        };
+        $c->stash->{posts}
+            = [ $c->model('DB')->get_posts_from_date( $year, $month ) ];
+    }
+    else {
+        $c->stash->{posts} = [];
+        $c->stash->{error_msg} = '올바르지 않은 날짜 표현입니다.';
+        return;
+    }
+
+    $c->stash->{title} = '저장소: ' . $c->{stash}->{date}->{str};
 }
 
 
