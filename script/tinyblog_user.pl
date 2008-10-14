2e9993f9b1e090b81885e2af6df77cbc2f9c9b19
keedi.k <keedi.k@cc9a5c7a-94df-11dd-9740-e5297458527a>
2008-10-14 16:12:19 +0000

    등록 사용자의 항목을 수정할 수 있는 기능 추가

--- script/tinyblog_user.pl
+++ script/tinyblog_user.pl
@@ -16,7 +16,7 @@ use TinyBlog::Schema;
 #
 die usage()
     unless @ARGV >= 2;
-my ( $action, $username ) = @ARGV;
+my ( $action, $username, @argv ) = @ARGV;
 
 #
 # DB 접속
@@ -48,6 +48,65 @@ if ( $action eq 'view' ) {
 
     printf "%15s: %s\n", $_, $param_of{$_}
         for qw/ username nick email active roles /;
+}
+elsif ( $action eq 'edit' ) {
+    my $user = $schema->resultset('Users')->find( { username => $username } )
+        or die "Cannot find [$username]";
+
+    my $field = shift @argv;
+    die usage() unless $field;
+
+    my $old_value;
+    if ( $field eq 'roles' ) {
+        $old_value = join ', ', map { $_->role } $user->roles;
+    }
+    else {
+        $old_value = eval "\$user->$field";
+        if ( $@ ) {
+            die "Getting $field failed: $@";
+        }
+    }
+
+    printf "Old: %15s: %s\n", $field, $old_value;
+    printf "New: %15s: ", $field;
+    my $new_value = <STDIN>;
+    chomp $new_value;
+
+    print "Check...\n";
+    printf "Old: %15s: %s\n", $field, $old_value;
+    printf "New: %15s: %s\n", $field, $new_value;
+
+    print "Edit $field field of $username? (yes/no): ";
+    my $confirm = <STDIN>;
+    chomp $confirm;
+
+    if ( $confirm =~ /^yes$/i ) {
+
+        if ( $field eq 'roles' ) {
+            my @role_names = map { s/^\s+//; s/\s+$//; $_ } split /,/, $new_value;
+            for my $role_name ( @role_names ) {
+
+                my $role = $schema->resultset('Roles')->find({ role => $role_name });
+                next unless $role;
+
+                my $user_role = $schema->resultset('UserRoles')->find_or_create( {
+                    user_id => $user->id,
+                    role_id => $role->id,
+                } );
+                $user_role->update;
+            }
+        }
+        else {
+            eval "\$user->$field($new_value)";
+            if ( $@ ) {
+                die "Editing $field field of $username failed: $@";
+            }
+        }
+        $user->update;
+    }
+    else {
+        die "Canceled [$action] process!\n";
+    }
 }
 elsif ( $action eq 'add' ) {
     my $user = $schema->resultset('Users')->find( { username => $username } )
@@ -132,8 +191,8 @@ else {
 
 sub usage {
     return <<"END_USAGE";
-Usage: $0 <action> <username>
+Usage: $0 <action> <username> [ <arg1> ... ]
 
-action: view, add, del
+action: view, add, del, edit
 END_USAGE
 }
